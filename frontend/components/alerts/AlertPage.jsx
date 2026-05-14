51ac982d27cd17abe9b205063dfba6e763e69992
ramkarode <ramkarode1982@gmail.com>
2026-05-14 12:23:30 +0530

    fix(AlertPage): change alert notification from toast.success to alert for immediate user feedback

--- frontend/components/alerts/AlertPage.jsx
+++ frontend/components/alerts/AlertPage.jsx
@@ -391,7 +391,8 @@ const AlertsPage = () => {
 
   useEffect(() => {
     socket.on("alert-triggered", (data) => {
-     toast.success("An alert was triggered!");
+    //  toast.success("An alert was triggered!");
+    alert("An alert was triggered!")
       setAlerts((prev) =>
         prev.map((alertItem) => {
           if (alertItem._id === data.data.alertId) {
